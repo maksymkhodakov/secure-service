3333fa7d0025c46c09376175657fba633b4eafbe
Jovakinn <max05012004@gmail.com>
2022-07-28 22:11:48 +0300

    corrected entities

--- src/main/java/com/example/security/domain/Role.java
+++ src/main/java/com/example/security/domain/Role.java
@@ -1,25 +1,43 @@
 package com.example.security.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-import static javax.persistence.GenerationType.AUTO;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Role {
-    @Id
-    @GeneratedValue(strategy = AUTO)
-    private Long id;
+@Setter
+@ToString
+public class Role extends AbstractEntity {
+
+    private String name;
 
     @Column(name = "name", unique = true)
-    private String name;
+    public String getName() {
+        return name;
+    }
+
+    public Role(Long id, LocalDateTime created, LocalDateTime updated, String name) {
+        super(id, created, updated);
+        this.name = name;
+    }
+
+    public Role() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Role role = (Role) o;
+        return getId() != null && Objects.equals(getId(), role.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- src/main/java/com/example/security/domain/User.java
+++ src/main/java/com/example/security/domain/User.java
@@ -1,35 +1,73 @@
 package com.example.security.domain;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 import static javax.persistence.FetchType.EAGER;
-import static javax.persistence.GenerationType.AUTO;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class User {
-    @Id
-    @GeneratedValue(strategy = AUTO)
-    private Long id;
+@Setter
+@ToString
+public class User extends AbstractEntity{
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "username", unique = true)
     private String username;
 
-    @Column(name = "password", nullable = false)
     private String password;
 
-    @ManyToMany(fetch = EAGER)
     private Collection<Role> roles = new ArrayList<>();
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    @Column(name = "username", unique = true)
+    public String getUsername() {
+        return username;
+    }
+
+    @Column(name = "password", nullable = false)
+    public String getPassword() {
+        return password;
+    }
+
+    @ManyToMany(fetch = EAGER)
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public User(Long id, LocalDateTime created, LocalDateTime updated, String name, String username, String password, Collection<Role> roles) {
+        super(id, created, updated);
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
