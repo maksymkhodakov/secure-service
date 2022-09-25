598f95e70a1f1f2f69ccd2de5813e0c0d0c7ab4d
Jovakinn <max05012004@gmail.com>
2022-09-25 21:31:02 +0300

    add hero repository

---
+++ src/main/java/com/example/security/repo/HeroRepo.java
@@ -0,0 +1,16 @@
+package com.example.security.repo;
+
+import com.example.security.domain.Hero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface HeroRepo extends JpaRepository<Hero, Long>, JpaSpecificationExecutor<Hero> {
+    @Query(nativeQuery = true, value = "" +
+            "SELECT * FROM heroes " +
+            "ORDER BY created DESC LIMIT 1;"
+    )
+    Optional<Hero> findLastCreatedHero();
+}
