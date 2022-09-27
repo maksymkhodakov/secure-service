a065878abd261db02a30526c73b5213b7494b76b
Jovakinn <max05012004@gmail.com>
2022-09-27 23:56:29 +0300

    add async functional

--- src/main/java/com/example/security/SecureUserServiceApplication.java
+++ src/main/java/com/example/security/SecureUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SecureUserServiceApplication {
 
     public static void main(String[] args) {
