8885b46f3d540f93f5cbe03f550821110799bc9e
Jovakinn <max05012004@gmail.com>
2023-01-02 17:35:14 +0200

    fix user repo of N + 1

--- src/main/java/com/example/security/repo/UserRepo.java
+++ src/main/java/com/example/security/repo/UserRepo.java
@@ -13,15 +13,15 @@ public interface UserRepo extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles")
     List<User> findAllUsers();
 
-    @Query("select u from User u where u.username = ?1")
+    @Query("select u from User u LEFT JOIN FETCH u.roles where u.username = :username")
     User findByUsername(String username);
 
-    @Query("select u from User u where u.name = ?1")
+    @Query("select u from User u LEFT JOIN FETCH u.roles where u.name = :name")
     Optional<List<User>> findByName(String name);
 
-    @Query("select u from User u where u.password = ?1")
+    @Query("select u from User u LEFT JOIN FETCH u.roles where u.password = :password")
     Optional<List<User>> findUserByPassword(String password);
 
-    @Query("select u from User u where u.created between ?1 and ?2")
+    @Query("select u from User u LEFT JOIN FETCH u.roles where u.created between :start and :end")
     Optional<User> findByCreatedBetween(LocalDateTime start, LocalDateTime end);
 }
